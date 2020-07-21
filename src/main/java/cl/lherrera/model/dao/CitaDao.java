11511eed723c841be2aac850f1efc52d4c19cc69
Luis Herrera <l.herrera.garnica@gmail.com>
2020-07-21 11:10:16 -0400

    proyecto finalizado, implementaciOn de RestTemplate para el consumo de api

--- src/main/java/cl/lherrera/model/dao/CitaDao.java
+++ src/main/java/cl/lherrera/model/dao/CitaDao.java
@@ -1,17 +1,38 @@
 package cl.lherrera.model.dao;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
 
 import cl.lherrera.model.entity.Cita;
 
 @Repository
 public class CitaDao {
+
+    @Value("${servicio.citas.url}")
+    private String urlServicio;
+    
+    // RestTemplate
+    @Autowired
+    private RestTemplate libreriaRestCient;
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    // RestTemplate...
+    
     
     public List<Cita> findAll() {
-        return new ArrayList<>();
+        Cita[] citasArray = libreriaRestCient.getForObject(urlServicio, Cita[].class);
+        
+        return Arrays.asList(citasArray);
     }
     
 }
